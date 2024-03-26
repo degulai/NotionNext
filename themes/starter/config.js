69bac6467da81199a4c881d89f3198366f9d8ba7
degulai <36154120+degulai@users.noreply.github.com>
2024-03-26 21:52:37 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -14,9 +14,9 @@ const CONFIG = {
   STARTER_HERO_TITLE_1: '中俄贸易服务平台', // 英雄区文字
   STARTER_HERO_TITLE_2: '助力中国中小企业出海', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_TEXT: '联系我们', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: '我们的故事', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
 
